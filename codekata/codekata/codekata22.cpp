13f4218759f6a6f997624e0fd8c55827ab6a0ba4
chagunwoo <dusrldlfrl1229@gmail.com>
2026-07-31 10:12:49 +0900

    codekata 22

---
+++ codekata/codekata/codekata22.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+    while (n != 1)
+    {
+        cout << n << endl;
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+            answer += 1;
+        }
+        else
+        {
+            n = n * 3 + 1;
+            answer += 1;
+        }
+        if (answer >= 500)
+        {
+            answer = -1;
+            break;
+        }
+    }
+    return answer;
+}
