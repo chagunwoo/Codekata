3f1abd799a0eb8f86f9cac0337875f7176631234
chagunwoo <dusrldlfrl1229@gmail.com>
2026-08-19 09:15:41 +0900

    codekata32

---
+++ codekata/codekata/codekata32.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+
+
+long long solution(int price, int money, int count)
+{
+    long long answer = -1;
+    long long sum = 0;
+    for (int i = 1; i <= count; i++)
+    {
+        sum += price * i;
+    }
+    if (0 > sum - money)
+    {
+        return 0;
+    }
+    answer = sum - money;
+    return answer;
+}
